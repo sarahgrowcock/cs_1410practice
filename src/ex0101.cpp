0e50c487e7acf8e07a44e5684d3d5cbaebf400f2
sarahgrowcock <sarahgrowcock@mail.weber.edu>
2025-09-14 19:55:46 +0000

    finished ex0101

---
+++ src/ex0101.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x, y;
+    cout << "Enter x: ";
+    cin >> x;
+
+    cout << "Enter y: ";
+    cin >> y;
+
+    cout << "x + y = " << (x + y) << endl;
+    cout << "x - y = " << (x - y) << endl;
+    cout << "x * y = " << (x * y) << endl;
+    cout << "x / y = " << (x / y) << endl;
+    
+    return 0;
+}
