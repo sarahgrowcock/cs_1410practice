347228abc381f798a1d9f258a40e95851a328dd8
sarahgrowcock <sarahgrowcock@mail.weber.edu>
2025-09-15 19:39:23 +0000

    Done with week 2

---
+++ src/ex0203.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double x, y;
+    char c;
+
+    do {
+        cout << "Enter operation (+, -, *, /) or q to terminate: ";
+        cin >> c;
+        if (c == 'q') break;
+
+        cout << "Enter x and y: ";
+        cin >> x >> y;
+        switch(c){
+            case '+': cout << x + y << endl; break;
+            case '-': cout << x - y << endl; break;
+            case '*': cout << x * y << endl; break;
+            case '/': cout << x / y << endl; break;
+        }
+
+    } while (c != 'q');
+    
+    return 0;
+}
